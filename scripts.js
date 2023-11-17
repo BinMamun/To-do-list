78735c82f8271accee145bc920c255ddc025c3cd
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-17 23:03:43 +0600

    savetoLocal

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const inputBox = document.querySelector(".js-input-box");
+let listContainer = document.querySelector(".js-list-container");
+const message = document.querySelector(".js-message");
+
+const addButton = document.querySelector(".js-add-new-button");
+
+listContainer.innerHTML = JSON.parse(localStorage.getItem("taskList")); //getting from localStorage
+
+addButton.addEventListener("click", () => {
+  if (inputBox.value === "") {
+    message.innerHTML = "Input field is empty";
+  }
+  else {
+    const li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    inputBox.value = "";
+    message.innerHTML = "";
+    saveToLocal(); //saving to loacal Storage
+  }
+})
+
+function saveToLocal() {
+  localStorage.setItem("taskList", JSON.stringify(listContainer.innerHTML));
+}
