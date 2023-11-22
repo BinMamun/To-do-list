af8cdb4fa1324113b258cedbcbb733b523badca3
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-22 14:59:33 +0600

    Add date input into the project

--- Script/localData.js
+++ Script/localData.js
@@ -1,9 +1,9 @@
 import { listContainer } from "./scripts.js";
 
-// export function saveToLocal() {
-//   localStorage.setItem("taskList", listContainer.innerHTML);
-// }
+export function saveToLocal() {
+  localStorage.setItem("taskList", listContainer.innerHTML);
+}
 
-// export function showList() {
-//   listContainer.innerHTML = localStorage.getItem("taskList"); //getting data from localStorage
-// }
+export function showList() {
+  listContainer.innerHTML = localStorage.getItem("taskList"); //getting data from localStorage
+}
--- Script/scripts.js
+++ Script/scripts.js
@@ -1,8 +1,6 @@
-// import { saveToLocal, showList } from "./localData.js"
+import { saveToLocal, showList } from "./localData.js"
 
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-
-console.log(dayjs());
 
 const inputBox = document.querySelector(".js-input-box");
 export let listContainer = document.querySelector(".js-list-container");
@@ -11,7 +9,7 @@ const dateInput = document.querySelector(".js-date-input");
 
 const addButton = document.querySelector(".js-add-new-button");
 
-//showList(); // display the previous list data after loading the page.
+showList(); // display the previous list data after loading the page.
 
 addButton.addEventListener("click", () => {
   if (inputBox.value === "") {
@@ -22,16 +20,21 @@ addButton.addEventListener("click", () => {
     const li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+
     const div = document.createElement("div");
-    div.innerHTML = dateInput.value;
-    li.appendChild(div);
-    // const span = document.createElement("span");
-    // span.innerHTML = "\u00d7";
-    // li.appendChild(span);
-    // inputBox.value = "";
-    // message.innerHTML = "";
-    console.log(listContainer);
-    //saveToLocal(); //saving to loacal Storage
+    const dateValue = dayjs(dateInput.value).format("D-MM-YYYY"); //formating date input value
+    div.innerHTML = dateValue;
+    li.appendChild(div); //adding date input into li
+    dateInput.value = "";
+
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span); //adding close into li
+
+    inputBox.value = "";
+    message.innerHTML = "";
+
+    saveToLocal(); //saving to loacal Storage
   }
 })
 
@@ -46,23 +49,32 @@ listContainer.addEventListener("click", (event) => {
 
 
 
-// inputBox.addEventListener("keydown", (event) => {
-//   if (event.key === "Enter") {
-//     if (inputBox.value === "") {
-//       message.innerHTML = "Input field is empty";
-//       message.classList.add("message");
-//     }
-//     else {
-//       const li = document.createElement("li");
-//       li.innerHTML = inputBox.value;
-//       listContainer.appendChild(li);
-//       const span = document.createElement("span");
-//       span.innerHTML = "\u00d7";
-//       li.appendChild(span);
-//       inputBox.value = "";
-//       message.innerHTML = "";
-//     }
-//     saveToLocal(); //saving to loacal Storage
-//   }
-// })
+dateInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    if (inputBox.value === "") {
+      message.innerHTML = "Input field is empty";
+      message.classList.add("message");
+    }
+    else {
+      const li = document.createElement("li");
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+
+      const div = document.createElement("div");
+      const dateValue = dayjs(dateInput.value).format("D-MM-YYYY"); //formating date input value
+      div.innerHTML = dateValue;
+      li.appendChild(div); //adding date input into li
+      dateInput.value = "";
+
+      const span = document.createElement("span");
+      span.innerHTML = "\u00d7";
+      li.appendChild(span); //adding close into li
+
+      inputBox.value = "";
+      message.innerHTML = "";
+
+    }
+    saveToLocal(); //saving to loacal Storage
+  }
+})
 
