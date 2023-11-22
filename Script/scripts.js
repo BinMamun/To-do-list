027c3ba9c0a0a19f5035110ee9e120387fc839db
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-22 14:08:09 +0600

    Styiling date input and list

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,59 +1,59 @@
-import { saveToLocal, showList } from "./localData.js"
+// import { saveToLocal, showList } from "./localData.js"
 
-const inputBox = document.querySelector(".js-input-box");
-export let listContainer = document.querySelector(".js-list-container");
-const message = document.querySelector(".js-message");
+// const inputBox = document.querySelector(".js-input-box");
+// export let listContainer = document.querySelector(".js-list-container");
+// const message = document.querySelector(".js-message");
 
-const addButton = document.querySelector(".js-add-new-button");
+// const addButton = document.querySelector(".js-add-new-button");
 
-showList(); // display the previous list data after loading the page.
+// //showList(); // display the previous list data after loading the page.
 
-addButton.addEventListener("click", () => {
-  if (inputBox.value === "") {
-    message.innerHTML = "Input field is empty";
-    message.classList.add("message");
-  }
-  else {
-    const li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    const span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-    inputBox.value = "";
-    message.innerHTML = "";
-    saveToLocal(); //saving to loacal Storage
-  }
-})
+// // addButton.addEventListener("click", () => {
+// //   if (inputBox.value === "") {
+// //     message.innerHTML = "Input field is empty";
+// //     message.classList.add("message");
+// //   }
+// //   else {
+// //     const li = document.createElement("li");
+// //     li.innerHTML = inputBox.value;
+// //     listContainer.appendChild(li);
+// //     const span = document.createElement("span");
+// //     span.innerHTML = "\u00d7";
+// //     li.appendChild(span);
+// //     inputBox.value = "";
+// //     message.innerHTML = "";
+// //     saveToLocal(); //saving to loacal Storage
+// //   }
+// // })
 
-listContainer.addEventListener("click", (event) => {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("checked");
-  } else if (event.target.tagName === "SPAN") {
-    event.target.parentElement.remove();
-  }
-  saveToLocal(); //saving to loacal Storage
-}, false);
+// listContainer.addEventListener("click", (event) => {
+//   if (event.target.tagName === "LI") {
+//     event.target.classList.toggle("checked");
+//   } else if (event.target.tagName === "SPAN") {
+//     event.target.parentElement.remove();
+//   }
+//   saveToLocal(); //saving to loacal Storage
+// }, false);
 
 
 
-inputBox.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    if (inputBox.value === "") {
-      message.innerHTML = "Input field is empty";
-      message.classList.add("message");
-    }
-    else {
-      const li = document.createElement("li");
-      li.innerHTML = inputBox.value;
-      listContainer.appendChild(li);
-      const span = document.createElement("span");
-      span.innerHTML = "\u00d7";
-      li.appendChild(span);
-      inputBox.value = "";
-      message.innerHTML = "";
-    }
-    saveToLocal(); //saving to loacal Storage
-  }
-})
+// inputBox.addEventListener("keydown", (event) => {
+//   if (event.key === "Enter") {
+//     if (inputBox.value === "") {
+//       message.innerHTML = "Input field is empty";
+//       message.classList.add("message");
+//     }
+//     else {
+//       const li = document.createElement("li");
+//       li.innerHTML = inputBox.value;
+//       listContainer.appendChild(li);
+//       const span = document.createElement("span");
+//       span.innerHTML = "\u00d7";
+//       li.appendChild(span);
+//       inputBox.value = "";
+//       message.innerHTML = "";
+//     }
+//     saveToLocal(); //saving to loacal Storage
+//   }
+// })
 
